487b0be2af8fc1824b6915d26c6314557b40f3d3
Julia Ogris <julia.ogris@gmail.com>
2022-08-23 11:47:52 +1000

    Add confetti easter egg
    Add confetti easter egg, display round divs (confetti) when source code
    contains substring confetti.
    
    Signed-off-by: Julia Ogris <julia.ogris@gmail.com>

--- frontend/index.js
+++ frontend/index.js
@@ -22,6 +22,9 @@ function jsPrint(ptr, len) {
   const str = new TextDecoder('utf8').decode(buf)
   const output = document.getElementById('output')
   output.textContent += str
+  if (str.toLowerCase().includes('confetti')) {
+    showConfetti()
+  }
 }
 
 // handleRun retrieves the input string from the source pane and
@@ -43,3 +46,54 @@ function handleRun() {
 }
 
 initWasm()
+
+// --------------------------------------------------
+// confetti easter egg
+// When source input string contains the sub string "confetti"
+// show confetti when clicking Run button.
+function showConfetti() {
+  const names = ['?']
+  const colors = ['red']
+  let confetti = new Array(10)
+    .fill()
+    .map((_, i) => {
+      return {
+        name: names[i % names.length],
+        x: Math.random() * 100,
+        y: -20 - Math.random() * 100,
+        r: 0.1 + Math.random() * 1,
+        color: colors[i % colors.length]
+      }
+    })
+    .sort((a, b) => a.r - b.r)
+
+  const cssText = (c) =>
+    `background: ${c.color};left: ${c.x}%; top: ${c.y}%; transform: scale(${c.r})`
+  const confettiDivs = confetti.map((c) => {
+    const div = document.createElement('div')
+    div.style.cssText = cssText(c)
+    div.classList.add('confetti')
+    div.textContent = c.name
+    document.body.appendChild(div)
+    return div
+  })
+
+  let frame
+
+  function loop() {
+    frame = requestAnimationFrame(loop)
+    confetti = confetti.map((c, i) => {
+      c.y += 0.7 * c.r
+      if (c.y > 120) c.y = -20
+      const div = confettiDivs[i]
+      div.style.cssText = cssText(c)
+      return c
+    })
+  }
+
+  loop()
+  setTimeout(() => {
+    cancelAnimationFrame(frame)
+    confettiDivs.forEach((div) => div.remove())
+  }, 10000)
+}

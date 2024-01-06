2e0251af1310e23bd421ab91097309fa800a6603
Julia Ogris <julia.ogris@gmail.com>
2024-01-07 07:03:13 +1100

    frontend: Make confetti.js self-contained
    Make confetti.js self-contained by removing the dependency on a separate CSS
    file.  This is done by embedding the CSS in the JS file as style object.

--- frontend/module/confetti.js
+++ frontend/module/confetti.js
@@ -14,12 +14,9 @@ export default function showConfetti() {
     })
     .sort((a, b) => a.r - b.r)
 
-  const cssText = (c) =>
-    `background: ${c.color}; left: ${c.x}%; top: ${c.y}%; transform: scale(${c.r})`
   const confettiDivs = confetti.map((c) => {
     const div = document.createElement("div")
-    div.style.cssText = cssText(c)
-    div.classList.add("confetti")
+    Object.assign(div.style, confettiStyle(c))
     div.textContent = c.name
     document.body.appendChild(div)
     return div
@@ -33,7 +30,7 @@ export default function showConfetti() {
       c.y += 0.7 * c.r
       if (c.y > 120) c.y = -20
       const div = confettiDivs[i]
-      div.style.cssText = cssText(c)
+      Object.assign(div.style, { top: "" + c.y + "%" })
       return c
     })
   }
@@ -44,6 +41,28 @@ export default function showConfetti() {
     confettiDivs.forEach((div) => div.remove())
   }, 10000)
   setTimeout(() => {
-    confettiDivs.forEach((div) => div.classList.add("fadeout"))
+    confettiDivs.forEach((div) =>
+      Object.assign(div.style, {
+        opacity: 0,
+        transition: "opacity 1.5s ease-in-out",
+      }),
+    )
   }, 8500)
+}
+
+function confettiStyle(confetti) {
+  return {
+    background: confetti.color,
+    left: "" + confetti.x + "%",
+    top: "" + confetti.y + "%",
+    transform: `scale(${confetti.r})`,
+    height: "7vw",
+    width: "7vw",
+    lineHeight: "7vw",
+    borderRadius: "50%",
+    position: "absolute",
+    fontSize: "4vw",
+    userSelect: "none",
+    textAlign: "center",
+  }
 }

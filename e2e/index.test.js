ac2882ebee59e77aa9ff15b3a09561cf4e601522
Julia Ogris <julia.ogris@gmail.com>
2024-01-21 19:27:48 +1100

    e2e: Add more snapshot end-to-end tests
    Add more snapshot end-to-end tests covering untested corners of the UI:
    sidemenu and dialog boxes.

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -9,17 +9,16 @@ test("console output", async ({ page, baseURL }) => {
   await page.goto(baseURL)
   await page.waitForLoadState("networkidle")
   await page.getByRole("button", { name: "Run" }).click()
-  const console = page.locator("css=#console")
-  await expect(console).toContainText("x: 12")
-  await expect(console).toContainText("🍦 big x")
+  await expect(page.locator("#console")).toContainText("x: 12 🍦 big x")
   await expect(page).toHaveScreenshot("console-output.png")
 })
 
 test("header navigation", async ({ page, baseURL }) => {
   await page.goto(baseURL)
   await page.waitForLoadState("networkidle")
-  const modal = page.locator("css=#modal")
+  const modal = page.locator("#modal")
   await expect(modal).toBeHidden()
+
   await page.getByRole("button", { name: "Welcome" }).click()
   await expect(modal).toBeVisible()
   await expect(modal).toContainText("🚌 Tour")
@@ -27,7 +26,55 @@ test("header navigation", async ({ page, baseURL }) => {
 
   await page.getByRole("link", { name: "Coordinates" }).click()
   await expect(modal).toBeHidden()
+  await expect(page.getByRole("textbox")).toHaveValue(
+    `grid
+print "Move mouse or touch to print coordinates"
 
-  const editor = page.locator("css=.editor")
-  await expect(editor).toContainText("on move x:num y:num")
+on move x:num y:num
+    print "x:" (round x) "y:" (round y)
+end
+`,
+  )
+})
+
+test("side menu", async ({ page, baseURL }) => {
+  await page.goto(baseURL)
+  await page.waitForLoadState("networkidle")
+
+  // show side menu
+  await page.locator("#hamburger").click()
+  await page.getByText("About Evy Docs Discord GitHub").click()
+  await expect(page).toHaveScreenshot("sidemenu.png")
+
+  // hide side menu by click on main
+  await page.getByRole("main").click()
+  await expect(page).toHaveScreenshot("no-sidemenu.png")
+
+  // show side menu again
+  await page.locator("#hamburger").click()
+  await page.getByText("About Evy Docs Discord GitHub").click()
+  await expect(page).toHaveScreenshot("sidemenu.png")
+
+  // hide side menu by click on top menu
+  await page.getByRole("button", { name: "Welcome" }).click()
+  await expect(page).toHaveScreenshot("modal.png")
+})
+
+test("dialogs", async ({ page, baseURL }) => {
+  await page.goto(baseURL)
+  await page.waitForLoadState("networkidle")
+  await expect(page).toHaveScreenshot("no-dialog.png")
+
+  // show side menu
+  await page.locator("#share").getByText("Share").click()
+  await page.locator('input[type="text"]').click()
+  await page.locator('input[type="text"]').press("ArrowRight")
+  await expect(page).toHaveScreenshot("share-dialog.png")
+  await page.getByRole("button", { name: "Done" }).click()
+  await expect(page).toHaveScreenshot("no-dialog.png")
+  await page.locator("#hamburger").click()
+  await page.getByRole("button", { name: "About Evy" }).click()
+  await expect(page).toHaveScreenshot("about-dialog.png")
+  await page.locator("header").filter({ hasText: "About" }).getByRole("button").click()
+  await expect(page).toHaveScreenshot("no-dialog.png")
 })

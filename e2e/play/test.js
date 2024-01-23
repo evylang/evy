77e3da52e017153b46bc22e52abd8db043b83f4d
Julia Ogris <julia.ogris@gmail.com>
2024-01-23 20:08:09 +1100

    e2e: Fix typo in comment
    Fix typo in comment around rendering bug in ios.

--- e2e/play/test.js
+++ e2e/play/test.js
@@ -93,7 +93,7 @@ end
       await expect(page).toHaveScreenshot("share-dialog.png", { maxDiffPixelRatio: 0.01 })
       await page.getByRole("button", { name: "Done" }).click()
       if (testInfo.project.name != "ios") {
-        //TODO: there is a rendering bug for this on ios, few snapshot diff, see https://github.com/evylang/todo/issues/50
+        //TODO: there is a rendering bug for this on ios, view snapshot diff, see https://github.com/evylang/todo/issues/50
         await expect(page).toHaveScreenshot("no-dialog.png")
       }
       await page.locator("#hamburger").click()
@@ -102,7 +102,7 @@ end
       await expect(page).toHaveScreenshot("about-dialog.png", { maxDiffPixelRatio: 0.01 })
       await page.locator("header").filter({ hasText: "About" }).getByRole("button").click()
       if (testInfo.project.name != "ios") {
-        //TODO: there is a rendering bug for this on ios, few snapshot diff, see https://github.com/evylang/todo/issues/50
+        //TODO: there is a rendering bug for this on ios, view snapshot diff, see https://github.com/evylang/todo/issues/50
         await expect(page).toHaveScreenshot("no-dialog.png")
       }
     })

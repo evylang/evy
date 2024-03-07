dd035469a17943ce3a0546ba03ba866913bc2d0d
Julia Ogris <julia.ogris@gmail.com>
2024-03-07 16:44:01 +1100

    docs: Animate sidebar
    On all screens animate subsection expansion and collapse within sidebar.
    Highlight current heading in sidebar, expand up until current heading or
    current page if no subheading active (via anchor / URL hash).
    
    On mobile slide sidebar in and out.
    
    Commit-group: 1/2

---
+++ frontend/preview/index.js
@@ -0,0 +1,87 @@
+// Open and close sidebar on mobile on hamburger click.
+const sidebar = document.querySelector("#sidebar")
+document.querySelector("#hamburger").onclick = showSidebar
+document.querySelector("#sidebar-close").onclick = hideSidebar
+window.addEventListener("hashchange", hideSidebar)
+
+// Expand sidebar subsection on chevron expander click.
+const expanders = [...document.querySelectorAll("#sidebar .expander")]
+expanders.map((el) => (el.onclick = expanderClick))
+
+// Highlight current page (sub-)heading in sidebar.
+highlightCurrent()
+window.addEventListener("hashchange", highlightCurrent)
+
+// Utilities
+function showSidebar() {
+  sidebar.classList.add("show")
+  document.addEventListener("click", handleOutsideSidebarClick)
+}
+function hideSidebar() {
+  sidebar.classList.remove("show")
+  document.removeEventListener("click", handleOutsideSidebarClick)
+}
+function handleOutsideSidebarClick(e) {
+  if (sidebar.classList.contains("show") && e.pageX > sidebar.offsetWidth) {
+    hideSidebar()
+  }
+}
+
+function expanderClick(e) {
+  e.target.nextElementSibling.classList.toggle("show")
+  e.target.classList.toggle("show")
+}
+
+function highlightCurrent() {
+  const highlighted = document.querySelectorAll("#sidebar .highlight")
+  highlighted.forEach((el) => el.classList.remove("highlight"))
+
+  const item = getCurrentItem()
+  if (!item) {
+    return
+  }
+  item.classList.add("highlight")
+  const els = getShowing(item)
+  els.forEach((el) => el.classList.add("show"))
+  const last = els.pop()
+  last && last.previousElementSibling.classList.add("highlight-within")
+}
+
+function getCurrentItem() {
+  const href = normalizedHref()
+  const last = href.split("/").pop()
+  const nodes = document.querySelectorAll(`#sidebar a[href$="${last}"]`)
+  return [...nodes].find((n) => n.href === href)
+}
+
+function normalizedHref() {
+  let href = window.location.href
+  let hash = window.location.hash
+  if (hash) {
+    href = href.replace(hash, "")
+  }
+  if (href.endsWith("/")) {
+    href = href + "index.html"
+  }
+  return href + hash
+}
+
+function getShowing(item) {
+  const parents = []
+  let n = item
+  if (n.nextElementSibling) {
+    n = n.nextElementSibling.nextElementSibling // sibling UL
+  }
+  while (n) {
+    if (n.tagName === "UL") {
+      const expander = n.previousElementSibling // div with ".expander" class
+      if (!expander.classList.contains("expander")) {
+        break
+      }
+      parents.push(n)
+      parents.push(expander)
+    }
+    n = n.parentElement
+  }
+  return parents
+}

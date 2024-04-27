563b63a7c845c86dd4e7f16ca48d96eb6ba22810
Cam Hutchison <camh@xdna.net>
2024-04-27 15:49:27 +1000

    evaluator: Fix some funny names from previous refactor
    Fix some funny names - "Valal" instead of "Val". It looks like a
    previous refactor that made values private was a little off in the
    substitutions used to rename some vars.

--- pkg/evaluator/evaluator.go
+++ pkg/evaluator/evaluator.go
@@ -389,8 +389,8 @@ func (e *Evaluator) evalFunccall(funcCall *parser.FuncCall) (value, error) {
 	if err != nil {
 		return nil, err
 	}
-	if returnValalue, ok := funcResult.(*returnVal); ok {
-		return returnValalue.V, nil
+	if returnValue, ok := funcResult.(*returnVal); ok {
+		return returnValue.V, nil
 	}
 	return &noneVal{}, nil
 }
@@ -492,7 +492,7 @@ func (e *Evaluator) newRange(f *parser.ForStmt) (ranger, error) {
 	case *mapVal:
 		order := make([]string, len(*v.Order))
 		copy(order, *v.Order)
-		mapRange := &mapRange{mapValal: v, cur: 0, order: order}
+		mapRange := &mapRange{mapVal: v, cur: 0, order: order}
 		if f.LoopVar != nil {
 			mapRange.loopVar = &stringVal{}
 			e.scope.set(f.LoopVar.Name, mapRange.loopVar)
@@ -503,15 +503,15 @@ func (e *Evaluator) newRange(f *parser.ForStmt) (ranger, error) {
 }
 
 func (e *Evaluator) newStepRange(r *parser.StepRange, loopVar *parser.Var) (ranger, error) {
-	start, err := e.numValalWithDefault(r.Start, 0.0)
+	start, err := e.numValWithDefault(r.Start, 0.0)
 	if err != nil {
 		return nil, err
 	}
-	stop, err := e.numValal(r.Stop)
+	stop, err := e.numVal(r.Stop)
 	if err != nil {
 		return nil, err
 	}
-	step, err := e.numValalWithDefault(r.Step, 1.0)
+	step, err := e.numValWithDefault(r.Step, 1.0)
 	if err != nil {
 		return nil, err
 	}
@@ -532,23 +532,23 @@ func (e *Evaluator) newStepRange(r *parser.StepRange, loopVar *parser.Var) (rang
 	return sRange, nil
 }
 
-func (e *Evaluator) numValal(n parser.Node) (float64, error) {
+func (e *Evaluator) numVal(n parser.Node) (float64, error) {
 	v, err := e.eval(n)
 	if err != nil {
 		return 0, err
 	}
-	numValal, ok := v.(*numVal)
+	numVal, ok := v.(*numVal)
 	if !ok {
 		return 0, newErr(n, fmt.Errorf("%w: expected number, found %v", ErrType, v))
 	}
-	return numValal.V, nil
+	return numVal.V, nil
 }
 
-func (e *Evaluator) numValalWithDefault(n parser.Node, defaultVal float64) (float64, error) {
+func (e *Evaluator) numValWithDefault(n parser.Node, defaultVal float64) (float64, error) {
 	if n == nil {
 		return defaultVal, nil
 	}
-	return e.numValal(n)
+	return e.numVal(n)
 }
 
 func (e *Evaluator) evalConditionalBlock(condBlock *parser.ConditionalBlock) (value, bool, error) {
